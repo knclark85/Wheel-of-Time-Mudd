4d09051b83f37ecab6c18efbaecb514d66435c01
Kenneth Clark <knclark85@gmail.com>
2013-06-24 19:36:15 -0600

    Initial Commit

---
+++ MUDD/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MUDD
+//
+//  Created by Clark on 6/24/13.
+//  Copyright (c) 2013 Ken Clark. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
+
